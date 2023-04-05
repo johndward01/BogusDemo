e2e642670d8b4653d87491162915966cc18d8431
johndward01 <johndward01@gmail.com>
2023-04-05 09:11:30 -0500

    Add project files.

---
+++ BogusDemo/Gender.cs
@@ -0,0 +1,38 @@
+﻿public enum Gender
+{
+    Male,
+    Female
+}
+
+/* OUTPUT:
+User Created! Id=0
+ *
+{
+  "Id": 0,
+  "FirstName": "Audrey",
+  "LastName": "Spencer",
+  "FullName": "Audrey Spencer",
+  "UserName": "Audrey_Spencer72",
+  "Email": "Audrey82@gmail.com",
+  "Avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/itstotallyamy/128.jpg",
+  "CartId": "863f9462-5b88-471f-b833-991d68db8c93",
+  "SSN": "923-88-4231",
+  "Gender": 0,
+  "Orders": [
+    {
+      "OrderId": 0,
+      "Item": "orange",
+      "Quantity": 8
+    },
+    {
+      "OrderId": 1,
+      "Item": "banana",
+      "Quantity": 2
+    },
+    {
+      "OrderId": 2,
+      "Item": "kiwi",
+      "Quantity": 9
+    }
+  ]
+} */
---
+++ BogusDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿using Bogus;
+
+for (int i = 0; i < 100; i++)
+{
+    var person = new Bogus.Person();
+    var fullName = $"{person.FirstName} {person.LastName}";
+    Console.WriteLine(fullName);
+}
+
+
+
